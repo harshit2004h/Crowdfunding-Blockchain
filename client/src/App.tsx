e9d625fd98c0639551e47720f3ba35faae73d2a5
harshit2004h <harshitsrivastava2004h@gmail.com>
2025-03-30 18:40:48 +0530

    Add CrowdChain contract using ThirdWeb

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,10 +1,12 @@
-import './App.css'
+import "./App.css";
+import Spline from "@splinetool/react-spline";
 
 function App() {
   return (
     <>
+      <Spline scene="https://prod.spline.design/SYZMnofaCmkTxDcy/scene.splinecode" />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
